b63893e0d36abdf1a110ee12d40f1a4232852c9b
Анастасия Озерова <72444468+oneozerova@users.noreply.github.com>
2021-04-18 17:54:23 +0100

    updated

--- mechanics.py
+++ mechanics.py
@@ -4,19 +4,20 @@ import RPi.GPIO as GPIO
 from gpiozero  import LED
 from time import sleep
 
-GPIO.setmode(GPIO.BCM)
-
-camera = PiCamera()
-camera.resolution = (640,640)
-
-camera.start_preview()
-camera.start_recording('/home/pi/Desktop/video3.h264')
-
 led1 = LED(25)
 led2 = LED(24)
 
 led1.on()
 led2.on()
+sleep(0.5)
+
+GPIO.setmode(GPIO.BCM)
+
+camera = PiCamera()
+camera.resolution = (640,640)
+sleep(0.5)
+camera.start_preview()
+camera.start_recording('/home/pi/Desktop/cube.h264')
 
 ControlPin = [7, 11, 13, 15]
 
@@ -38,7 +39,7 @@ for i in range(512):
     for halfstep in range(8):
         for pin in range(4):
             GPIO.output(ControlPin[pin], seq[halfstep][pin])
-        time.sleep(0.001)
+        time.sleep(0.005)
 
 GPIO.cleanup()
 
